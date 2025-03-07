aae58e51042371e017fd79198a50173132e9b77c
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-03-07 10:32:07 +0800

    refactor: 重构TextReaderToolWindow, 不再使用com.intellij.ui.dsl

--- src/main/kotlin/com/muedsa/jetbrains/textReader/toolWindow/TextReaderToolWindow.kt
+++ src/main/kotlin/com/muedsa/jetbrains/textReader/toolWindow/TextReaderToolWindow.kt
@@ -6,20 +6,17 @@ import com.intellij.openapi.wm.ToolWindow
 import com.intellij.ui.components.JBList
 import com.intellij.ui.components.JBTabbedPane
 import com.intellij.ui.components.JBTextField
-import com.intellij.ui.dsl.builder.Align
-import com.intellij.ui.dsl.builder.panel
-import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
 import com.muedsa.jetbrains.textReader.model.SimpleChapterInfo
 import com.muedsa.jetbrains.textReader.services.TextReaderService
 import com.muedsa.jetbrains.textReader.setting.TextReaderSettings
+import java.awt.BorderLayout
+import java.awt.Component
+import java.awt.Dimension
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
-import javax.swing.Box
-import javax.swing.JPanel
-import javax.swing.JTextPane
-import javax.swing.SwingUtilities
+import javax.swing.*
 import javax.swing.text.SimpleAttributeSet
 import javax.swing.text.StyleConstants
 import javax.swing.text.StyledDocument
@@ -30,11 +27,65 @@ class TextReaderToolWindow(
 
     private val readerService = TextReaderService.getInstance()
 
-    private lateinit var chapterSearchTextField: JBTextField
+    val panel = JBTabbedPane().also {
+        it.font = JBFont.regular()
+        it.background = JBUI.CurrentTheme.ComplexPopup.HEADER_BACKGROUND
+        it.isFocusable = false
+    }
 
     private var chapterListData: Array<SimpleChapterInfo> = emptyArray()
 
-    private val chapterListComponent = JBList<SimpleChapterInfo>().apply {
+    private val chapterSearchTextField: JBTextField = JBTextField()
+        .apply {
+            addActionListener {
+                if (it.source is JBTextField) {
+                    val search = (it.source as JBTextField).text
+                    if (search.isNotEmpty()) {
+                        val listData = chapterListData.filter {
+                            val title = it.title
+                            title != null && title.contains(search)
+                        }.toTypedArray()
+                        chapterList.setListData(listData)
+                    } else {
+                        chapterList.setListData(chapterListData)
+                    }
+                }
+            }
+        }
+
+    private val chapterListControlRow = JPanel().also {
+        it.layout = BoxLayout(it, BoxLayout.X_AXIS)
+        val fileButton = JButton("File").apply {
+            addActionListener {
+                val file = FileChooser.chooseFile(
+                    FileChooserDescriptor(
+                        /* chooseFiles = */ true,
+                        /* chooseFolders = */ false,
+                        /* chooseJars = */ false,
+                        /* chooseJarsAsFiles = */ false,
+                        /* chooseJarContents = */ false,
+                        /* chooseMultiple = */ false,
+                    ).withFileFilter { it.extension == "txt" },
+                    toolWindow.project,
+                    null
+                )
+                if (file != null) {
+                    readerService.loadFile(file = file, project = toolWindow.project)
+                }
+            }
+        }
+        val clearButton = JButton("Clear").apply {
+            addActionListener {
+                readerService.clear()
+            }
+        }
+        it.add(fileButton)
+        it.add(clearButton)
+        it.add(Box.createHorizontalGlue())
+        it.add(chapterSearchTextField)
+    }
+
+    private val chapterList = JBList<SimpleChapterInfo>().apply {
         addMouseListener(object : MouseAdapter() {
             override fun mouseClicked(e: MouseEvent) {
                 if (SwingUtilities.isLeftMouseButton(e) && e.clickCount == 2) {
@@ -70,86 +121,38 @@ class TextReaderToolWindow(
         })
     }
 
-    private val contentTextComponent = JTextPane()
-
-    private val titlePanel = panel {
-        row {
-            button("File") {
-                val file = FileChooser.chooseFile(
-                    FileChooserDescriptor(
-                        /* chooseFiles = */ true,
-                        /* chooseFolders = */ false,
-                        /* chooseJars = */ false,
-                        /* chooseJarsAsFiles = */ false,
-                        /* chooseJarContents = */ false,
-                        /* chooseMultiple = */ false,
-                    ).withFileFilter { it.extension == "txt" },
-                    toolWindow.project,
-                    null
-                )
-                if (file != null) {
-                    readerService.loadFile(file = file, project = toolWindow.project)
-                }
-            }
-            button("Clear") {
-                readerService.clear()
-            }
-            cell(Box.createHorizontalBox().apply {
-                add(Box.createHorizontalGlue())
-            })
-            textField()
-                .also {
-                    chapterSearchTextField = it.component
-                    chapterSearchTextField.addActionListener {
-                        val search = chapterSearchTextField.text
-                        if (search.isNotEmpty()) {
-                            val listData = chapterListData.filter {
-                                val title = it.title
-                                title != null && title.contains(search)
-                            }.toTypedArray()
-                            chapterListComponent.setListData(listData)
-                        } else {
-                            chapterListComponent.setListData(chapterListData)
-                        }
-                    }
-                }
-                .horizontalAlign(HorizontalAlign.RIGHT)
-        }
-        row {
-            scrollCell(chapterListComponent)
-                .align(Align.FILL)
-                .resizableColumn()
-        }.resizableRow()
+    private val titlePanel = JPanel(BorderLayout()).also {
+        it.add(chapterListControlRow, BorderLayout.NORTH)
+        it.add(JScrollPane(chapterList), BorderLayout.CENTER)
     }
 
-    private val contentPanel = panel {
-        row {
-            button("Previous") {
+    private val chapterContentControlRow = JPanel().also {
+        it.layout = BoxLayout(it, BoxLayout.X_AXIS)
+        val previousButton = JButton("Previous").apply {
+            maximumSize = Dimension(Int.MAX_VALUE, preferredSize.height)
+            alignmentX = Component.CENTER_ALIGNMENT
+            addActionListener {
                 readerService.previousChapter()
             }
-                .align(Align.FILL)
-                .resizableColumn()
-            button("Next") {
+        }
+        val nextButton = JButton("Previous").apply {
+            maximumSize = Dimension(Int.MAX_VALUE, preferredSize.height)
+            alignmentX = Component.CENTER_ALIGNMENT
+            addActionListener {
                 readerService.nextChapter()
             }
-                .align(Align.FILL)
-                .resizableColumn()
         }
-        row {
-            scrollCell(contentTextComponent)
-                .align(Align.FILL)
-                .applyToComponent {
-                    isEditable = false
-                }
-        }.resizableRow()
+        it.add(previousButton)
+        it.add(Box.createHorizontalGlue())
+        it.add(nextButton)
     }
 
-    val panel = JBTabbedPane()
-        .apply {
-            font = JBFont.regular()
-            background = JBUI.CurrentTheme.ComplexPopup.HEADER_BACKGROUND
-            isFocusable = false
-        }
+    private val chapterContentText = JTextPane()
+
+    private val contentPanel = JPanel(BorderLayout()).also {
+        it.add(chapterContentControlRow, BorderLayout.NORTH)
+        it.add(JScrollPane(chapterContentText), BorderLayout.CENTER)
+    }
 
     init {
         panel.add("Title", titlePanel)
@@ -161,22 +164,22 @@ class TextReaderToolWindow(
     fun updateChapterList(list: List<SimpleChapterInfo>) {
         chapterSearchTextField.text = ""
         chapterListData = list.toTypedArray()
-        chapterListComponent.setListData(chapterListData)
+        chapterList.setListData(chapterListData)
     }
 
     fun updateChapter(text: String, settings: TextReaderSettings.State) {
-        if (chapterSearchTextField.text.isNotEmpty() && chapterListComponent.itemsCount < chapterListData.size) {
+        if (chapterSearchTextField.text.isNotEmpty() && chapterList.itemsCount < chapterListData.size) {
             chapterSearchTextField.text = ""
-            chapterListComponent.setListData(chapterListData)
+            chapterList.setListData(chapterListData)
         }
-        contentTextComponent.text = text
-        contentTextComponent.setCaretPosition(0)
+        chapterContentText.text = text
+        chapterContentText.setCaretPosition(0)
         val attributes = SimpleAttributeSet()
         StyleConstants.setFontFamily(attributes, settings.fontFamily)
         StyleConstants.setFontSize(attributes, settings.fontSize)
         StyleConstants.setLineSpacing(attributes, settings.lineSpace.toFloat())
         StyleConstants.setFirstLineIndent(attributes, settings.firstLineIndent.toFloat() * settings.fontSize.toFloat())
-        val styledDocument: StyledDocument = contentTextComponent.styledDocument
+        val styledDocument: StyledDocument = chapterContentText.styledDocument
         styledDocument.setParagraphAttributes(0, styledDocument.length, attributes, false)
     }
 }
